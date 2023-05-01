9cd3a040995912cd028ce115751d0afe4c04e78a
isatok <37401151+isatok@users.noreply.github.com>
2023-05-01 16:37:36 +0100

    Create 3_GLfix.sh

---
+++ Phasing/3_GLfix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -J GLfix
+#SBATCH -o /scratch/project_2001443/vcf/phasing/whatshap/GLfix.out
+#SBATCH -e /scratch/project_2001443/vcf/phasing/whatshap/GLfix.err
+#SBATCH --account=project_2001443
+#SBATCH -t 08:00:00
+#SBATCH -p small
+#SBATCH --ntasks 1
+#SBATCH --mem=6G
+
+cd /scratch/project_2001443/vcf/phasing/whatshap
+module load biokit
+
+for i in *.phased.vcf.gz ; do
+  echo $i
+  mv $i ${i}.tmp
+  bcftools view ${i}.tmp -Ov | perl -pi -e 's/(\#\#FORMAT=<ID=GL,Number=)G/$1\./' | bgzip > $i
+  bcftools index -t $i
+done
+
+###end
+3_GLfix.sh (END)
