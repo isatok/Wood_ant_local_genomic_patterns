298978edde38142a140548b1afd155993f50f55d
isatok <37401151+isatok@users.noreply.github.com>
2023-05-01 15:59:18 +0100

    Create Twisst_sympatric_parentals.sh

---
+++ Twisst_sympatric_parentals.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH -J twisst_symparsamples_exsog_Mi50
+#SBATCH --account=project_2001443
+#SBATCH -t 10:00:00
+#SBATCH -p small
+#SBATCH --ntasks=4
+#SBATCH --mem=8G
+
+cd /scratch/project_2001443/analysis/twisst
+export PATH="/projappl/project_2001443/ete3env/bin:$PATH"
+
+
+for x in 50; do
+
+echo "Running Twisst for window size $x"
+
+python3.6 /projappl/project_2001443/twisst/twisst.py \
+-t "trees/exs/phased_exs_out_max50percMi.phyml_bionj.w"$x".trees.gz" \
+-w "results/parentals/exs_outgroup/symparsamples_exs_og_max50percMi.w"$x".weights.csv.gz" \
+-g pratensis_fi \
+-g aquilonia_fi \
+-g rufa_fi \
+-g lugubris_fi \
+-g exsecta \
+--outgroup exsecta \
+--groupsFile group_symparsamples.tab
+
+done
+twisst_symparsamples_exsog_50Mi.sh (END)
