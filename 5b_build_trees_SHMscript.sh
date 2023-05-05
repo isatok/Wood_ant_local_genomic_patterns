59e1a841d4dce11b95244f12b29ae74b37e5ccfd
isatok <37401151+isatok@users.noreply.github.com>
2023-05-05 23:07:45 +0100

    Update 5b_build_trees_SHMscript.sh

--- 5b_build_trees_SHMscript.sh
+++ 5b_build_trees_SHMscript.sh
@@ -8,7 +8,7 @@ GENO=phased.exsecta.geno.gz
 PLOIDYTAB=/scratch/project_2001443/vcf/ind_lists/ploidy.tab
 
 export PYTHONPATH="/scratch/project_2001443/analysis/genomics_simon/genomics_general:$PYTHONPATH"
-export PATH="/projappl/project_2001443/ete3env/bin:$PATH"
+export PATH="/projappl/project_2001443/ete3env/bin:$PATH" #This includes NUMPY
 
   
   python3 /scratch/project_2001443/analysis/genomics_simon/genomics_general/filterGenotypes.py \
@@ -19,3 +19,9 @@ export PATH="/projappl/project_2001443/ete3env/bin:$PATH"
   -o $DATADIR/phased.exsecta.bases.geno.gz
    
  ## next run freq.py for this  $DATADIR/phased.exsecta.bases.geno.gz file. find out about how to do it.
+ 
+ python3 /scratch/project_2001443/analysis/genomics_simon/genomics_general/freq.py \
+ -g $DATADIR/phased.exsecta.bases.geno.gz --target derived --threads 10 --asCounts TRUE \
+ -o $DATADIR/phased.exsecta.bases.csv --popsFile
+
+#MODIFY EG PLOIDYTAB SO IT IS AS POPSFILE (SAMPLENAME SAMPLENAME)
