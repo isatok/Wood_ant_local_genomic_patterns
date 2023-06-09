55de40ae604e6aff0708d38cc9f8aec14c5b3b79
isatok <37401151+isatok@users.noreply.github.com>
2023-06-09 23:13:56 +0300

    Update 1_trim.sh

--- New_raw_data_to_bams/1_trim.sh
+++ New_raw_data_to_bams/1_trim.sh
@@ -7,11 +7,11 @@
 #SBATCH --account=project_2001443
 #SBATCH -t 12:00:00
 #SBATCH -p small
-#SBATCH --array=1-71
+#SBATCH --array=1-12
 #SBATCH --ntasks 4
 #SBATCH --mem=4G
 
-cd /scratch/project_2001443/fastq
+cd /scratch/project_2001443/barriers_introgr_formica/fastq
 
 # Get file name
 file=$(sed -n "$SLURM_ARRAY_TASK_ID"p name.list)
@@ -23,7 +23,7 @@ module load trimmomatic/0.39
 
 # Trim reads
 trimmomatic PE -threads 4 -phred33 \
-raw/R1/$file"_R1_001.fastq.gz" raw/R2/$file"_R2_001.fastq.gz" \
+raw/combined/$file"_1.fq.gz" raw/combined/$file"_2.fq.gz" \
 trim/$file"_1.trim.pair.fq.gz" trim/unpair/$file"_1.trim.unpair.fq.gz" \
 trim/$file"_2.trim.pair.fq.gz" trim/unpair/$file"_2.trim.unpair.fq.gz" \
 ILLUMINACLIP:/scratch/project_2001443/fastq/adapters/general_adapters.fa:2:30:10:2:keepBothReads LEADING:10 TRAILING:10 S
