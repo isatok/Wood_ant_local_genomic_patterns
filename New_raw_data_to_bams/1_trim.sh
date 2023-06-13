e0b9fec786c034e4c194621ef626464f34a1fa5d
isatok <37401151+isatok@users.noreply.github.com>
2023-06-13 16:57:08 +0300

    Update 1_trim.sh

--- New_raw_data_to_bams/1_trim.sh
+++ New_raw_data_to_bams/1_trim.sh
@@ -9,7 +9,7 @@
 #SBATCH -p small
 #SBATCH --array=1-12
 #SBATCH --ntasks 4
-#SBATCH --mem=4G
+#SBATCH --mem=6G
 
 cd /scratch/project_2001443/barriers_introgr_formica/fastq
 
@@ -23,10 +23,11 @@ module load trimmomatic/0.39
 
 # Trim reads
 trimmomatic PE -threads 4 -phred33 \
+-trimlog trim/logs/${file}.trim.log \
 raw/combined/$file"*1.fq.gz" raw/combined/$file"*2.fq.gz" \
 trim/$file"_1.trim.pair.fq.gz" trim/unpair/$file"_1.trim.unpair.fq.gz" \
 trim/$file"_2.trim.pair.fq.gz" trim/unpair/$file"_2.trim.unpair.fq.gz" \
-ILLUMINACLIP:/scratch/project_2001443/barriers_introgr_formica/fastq/adapters/nebnext_adapters.fa:2:30:10:2:keepBothReads LEADING:10 TRAILING:10 SLIDINGWINDOW:4:15 MINLEN:50
+ILLUMINACLIP:/scratch/project_2001443/barriers_introgr_formica/fastq/adapters/TruSeq3-PE_and_NebNext.fa:2:30:10:2:keepBothReads LEADING:10 TRAILING:10 SLIDINGWINDOW:4:15 MINLEN:50
 
 # Quality control
 fastqc trim/$file"_1.trim.pair.fq.gz" -o trim/fastqc && \
@@ -36,4 +37,5 @@ fastqc trim/$file"_2.trim.pair.fq.gz" -o trim/fastqc
 
 #NOTE: For library preparation NEBNext kit was used, and accordingly the adaptor sequences are
 #from the NEBNext manual (the kit: NEBNext® Ultra™ II FS DNA Library Prep Kit for Illumina (E7805L) 
-# + NEBNext® Multiplex Oligos for Illumina® (Dual Index Primers Set 1, E7600S))
+# + NEBNext® Multiplex Oligos for Illumina® (Dual Index Primers Set 1, E7600S)), supplemented with the TruSeq 3
+#adapters from Trimmomatic website, which are actually reverse complements of the NEBNext sequences.
