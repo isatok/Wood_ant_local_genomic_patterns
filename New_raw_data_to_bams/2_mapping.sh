37f1feb5e9b64e65549cea63f8c7372782776284
isatok <37401151+isatok@users.noreply.github.com>
2023-06-21 15:58:40 +0300

    Update 2_mapping.sh

--- New_raw_data_to_bams/2_mapping.sh
+++ New_raw_data_to_bams/2_mapping.sh
@@ -85,3 +85,35 @@ samtools idxstats -@4 $BAMPATH/nodupl/${shortfile}"_nodupl.bam" > $BAMPATH/nodup
 echo "###### DONE! sample ID: $file"
 
 ### This is the end.
+
+
+
+### Collect mean insert sizes per sample  --------------------
+
+cd /scratch/project_2001443/barriers_introgr_formica/bam/stats
+
+for i in *_insert_size_metrics.txt
+ do echo $i ; awk 'FNR == 8 {print $6}' $i
+done > all.mean_insert_sizes.tmp
+
+grep -v 'txt' all.mean_insert_sizes.tmp > tmp1
+grep 'txt' all.mean_insert_sizes.tmp | perl -npe 's/_insert_size_metrics.txt//' > tmp2
+paste tmp2 tmp1 > all.mean_insert_sizes.txt ; rm *tmp*
+
+scp satokan1@puhti.csc.fi:'/scratch/project_2001443/barriers_introgr_formica/bam/stats/all.mean_insert_sizes.txt' \
+'/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/mapping_and_insert_stats/'
+
+### Collect mapping rates per sample  --------------------
+
+cd /scratch/project_2001443/barriers_introgr_formica/bam/nodupl/stats
+
+for i in *_nodupl.flagstat
+ do echo $i ; awk 'FNR == 7' $i
+done > all.mapping_rates.tmp
+
+grep -v 'flagstat' all.mapping_rates.tmp > tmp1
+grep 'flagstat' all.mapping_rates.tmp | perl -npe 's/_nodupl.flagstat//' > tmp2
+paste tmp2 tmp1 > all.mapping_rates.txt ; rm *tmp*
+
+scp satokan1@puhti.csc.fi:'/scratch/project_2001443/barriers_introgr_formica/bam/nodupl/stats/all.mapping_rates.txt' \
+'/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/mapping_and_insert_stats/'
