f375c139c3cfa18ac5b370f72d65e6a7a84eed53
isatok <37401151+isatok@users.noreply.github.com>
2023-06-27 23:18:10 +0300

    Create 2b_mapping_paralugubris.sh

---
+++ New_raw_data_to_bams/2b_mapping_paralugubris.sh
@@ -0,0 +1,119 @@
+#!/bin/bash -l
+#SBATCH -J map_paralugubris
+#SBATCH -o /scratch/project_2001443/paralugubris/bam/logs/map_%j.out
+#SBATCH -e /scratch/project_2001443/paralugubris/bam/logs/map_%j.err
+#SBATCH --account=project_2001443
+#SBATCH -t 8:00:00
+#SBATCH -p small
+#SBATCH --array=1-2
+#SBATCH --ntasks 8
+#SBATCH --mem=12G
+#SBATCH --mail-user=ina.satokangas@helsinki.fi
+#SBATCH --mail-type=END
+
+
+###
+### 0. Prep -------------------------------------------------------------------
+###
+
+# load modules
+module load biokit
+module load r-env
+export PATH="/projappl/project_2001443/picardenv/bin:$PATH"  #use picard v 2.21.4, which was used for my other data as well 
+
+# Set work directory
+cd /scratch/project_2001443/paralugubris/
+
+# Define paths
+REFPATH=/scratch/project_2001443/reference_genome
+FASTQPATH=/scratch/project_2001443/paralugubris/fastq/trim
+BAMPATH=/scratch/project_2001443/paralugubris/bam
+
+# Get file name and sample ID
+file=$(sed -n "$SLURM_ARRAY_TASK_ID"p fastq/name.list)
+shortfile=${file}
+
+echo "###### STARTING file: $file"
+
+
+###
+### 1. Map, sort & index ------------------------------------------------------
+###
+
+echo "###### MAPPING"
+
+bwa mem -t8 $REFPATH/Formica_hybrid_v1_wFhyb_Sapis.fa $FASTQPATH/$file"_1.trim.pair.fq.gz" $FASTQPATH/$file"_2.trim.pair.fq.gz" | samtools sort -@8 -m512M -o $BAMPATH/raw/${shortfile}".bam" -
+
+samtools index -@4 $BAMPATH/raw/${shortfile}".bam"
+
+
+###
+### 2. Compute insert size distribution ---------------------------------------
+###
+
+echo "###### COLLECTING INSERT SIZES"
+
+picard CollectInsertSizeMetrics \
+I=$BAMPATH/raw/${shortfile}".bam" \
+O=$BAMPATH/stats/${shortfile}"_insert_size_metrics.txt" \
+H=$BAMPATH/stats/${shortfile}"_insert_size_hist.pdf"
+
+
+###
+### 3. Filter duplicates ----------------------------------------------------
+###
+
+echo "###### FILTERING DUPLICATES"
+
+picard MarkDuplicates \
+I=$BAMPATH/raw/${shortfile}".bam" \
+O=$BAMPATH/nodupl/${shortfile}"_nodupl.bam" \
+M=$BAMPATH/nodupl/stats/${shortfile}"_dupl_metrics.txt" \
+REMOVE_DUPLICATES=T
+
+
+###
+### 4. Index and compute stats ------------------------------------------------
+###
+
+echo "###### INDEX & GET FLAGSTATS"
+
+samtools index -@4 $BAMPATH/nodupl/${shortfile}"_nodupl.bam" && \
+samtools flagstat -@4 $BAMPATH/nodupl/${shortfile}"_nodupl.bam" > $BAMPATH/nodupl/stats/${shortfile}"_nodupl.flagstat" && \
+samtools idxstats -@4 $BAMPATH/nodupl/${shortfile}"_nodupl.bam" > $BAMPATH/nodupl/stats/${shortfile}"_nodupl.idxstat"
+
+echo "###### DONE! sample ID: $file"
+
+### This is the end.
+
+
+
+### Collect mean insert sizes per sample  --------------------
+
+cd /scratch/project_2001443/paralugubris/bam/stats
+
+for i in *_insert_size_metrics.txt
+ do echo $i ; awk 'FNR == 8 {print $6}' $i
+done > paralugubris.mean_insert_sizes.tmp
+
+grep -v 'txt' paralugubris.mean_insert_sizes.tmp > tmp1
+grep 'txt' paralugubris.mean_insert_sizes.tmp | perl -npe 's/_insert_size_metrics.txt//' > tmp2
+paste tmp2 tmp1 > paralugubris.mean_insert_sizes.txt ; rm *tmp*
+
+scp satokan1@puhti.csc.fi:'/scratch/project_2001443/paralugubris/bam/stats/paralugubris.mean_insert_sizes.txt' \
+'/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/mapping_and_insert_stats/'
+
+### Collect mapping rates per sample  --------------------
+
+cd /scratch/project_2001443/paralugubris/bam/nodupl/stats
+
+for i in *_nodupl.flagstat
+ do echo $i ; awk 'FNR == 7' $i
+done > paralugubris.mapping_rates.tmp
+
+grep -v 'flagstat' paralugubris.mapping_rates.tmp > tmp1
+grep 'flagstat' paralugubris.mapping_rates.tmp | perl -npe 's/_nodupl.flagstat//' > tmp2
+paste tmp2 tmp1 > paralugubris.mapping_rates.txt ; rm *tmp*
+
+scp satokan1@puhti.csc.fi:'/scratch/project_2001443/paralugubris/bam/nodupl/stats/paralugubris.mapping_rates.txt' \
+'/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/mapping_and_insert_stats/'
