8633fb716961c8b27c0c95737035cc9a9c1ed882
isatok <37401151+isatok@users.noreply.github.com>
2023-06-29 14:52:40 +0300

    Update 5b_build_trees_SHMscript.sh

--- 5b_build_trees_SHMscript.sh
+++ 5b_build_trees_SHMscript.sh
@@ -1,6 +1,12 @@
+#update Simon's scripts if needed: 
+cd /scratch/project_2001443/analysis/genomics_simon
+git clone https://github.com/simonhmartin/genomics_general.git
+
+---
+
 cd /scratch/project_2001443/vcf/geno
 
-sinteractive ...
+sinteractive --account project_2001443 --mem 6000
 
 DATADIR=/scratch/project_2001443/vcf/geno
 RESDIR=/scratch/project_2001443/analysis/twisst/trees/exs
@@ -8,7 +14,7 @@ GENO=phased.exsecta.geno.gz
 PLOIDYTAB=/scratch/project_2001443/vcf/ind_lists/ploidy.tab
 
 export PYTHONPATH="/scratch/project_2001443/analysis/genomics_simon/genomics_general:$PYTHONPATH"
-export PATH="/projappl/project_2001443/ete3env/bin:$PATH" #This includes NUMPY
+export PATH="/projappl/project_2001443/ete3env/bin:$PATH" #This contains NUMPY
 
   
   python3 /scratch/project_2001443/analysis/genomics_simon/genomics_general/filterGenotypes.py \
@@ -17,11 +23,12 @@ export PATH="/projappl/project_2001443/ete3env/bin:$PATH" #This includes NUMPY
   --ploidyFile /scratch/project_2001443/vcf/ind_lists/ploidy.tab \
   --outputGenoFormat bases \
   -o $DATADIR/phased.exsecta.bases.geno.gz
-   
- ## next run freq.py for this  $DATADIR/phased.exsecta.bases.geno.gz file. find out about how to do it.
  
  python3 /scratch/project_2001443/analysis/genomics_simon/genomics_general/freq.py \
  -g $DATADIR/phased.exsecta.bases.geno.gz --target derived --threads 10 --asCounts TRUE \
  -o $DATADIR/phased.exsecta.bases.csv --popsFile
 
-#MODIFY EG PLOIDYTAB SO IT IS AS POPSFILE (SAMPLENAME SAMPLENAME)
+#### build trees with sticcs.py
+
+#input file: /scratch/project_2001443/vcf/geno/phased.exsecta.bases.csv
+
