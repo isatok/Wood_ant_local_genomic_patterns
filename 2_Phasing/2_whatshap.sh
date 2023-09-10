a1e721f5800d6e5c7dc3cb36894d5b701f8290b7
isatok <37401151+isatok@users.noreply.github.com>
2023-09-10 14:13:55 +0300

    Update 2_whatshap.sh

--- 2_Phasing/2_whatshap.sh
+++ 2_Phasing/2_whatshap.sh
@@ -1,18 +1,18 @@
 #Create and ind list of the used vcf:
 cd /scratch/project_2001443/barriers_introgr_formica/vcf/filt
 bcftools query -l all_samples.DP8.hwe.AN10.noScaff00.mac2.vcf.gz > all_samples.DP8.hwe.AN10.noScaff00.mac2.ind.list
-# contains all samples except 110 and 105 (even does contain 54, Pus2, s353 and s354)
+# contains 101 samples; all except 110 and 105 (even does contain 54, Pus2, s353 and s354)
 
 ---
 
 #!/bin/bash -l
 #SBATCH -J whap
-#SBATCH -o /scratch/project_2001443/vcf/phasing/whatshap/logs/whatshap_%a.out ##CHANGE
-#SBATCH -e /scratch/project_2001443/vcf/phasing/whatshap/logs/whatshap_%a.err ##CHANGE
+#SBATCH -o /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/whatshap/logs/whatshap_%a.out
+#SBATCH -e /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/whatshap/logs/whatshap_%a.err
 #SBATCH --account=project_2001443
-#SBATCH -t 24:00:00
+#SBATCH -t 05:00:00
 #SBATCH -p small
-#SBATCH --array=1-89 ##CHANGE
+#SBATCH --array=1-101
 #SBATCH --ntasks 1
 #SBATCH --mem=3G
 
@@ -26,10 +26,11 @@ cd /scratch/project_2001443/barriers_introgr_formica/vcf/phasing
 BAMDIR=/scratch/project_2001443/barriers_introgr_formica/bam_all
 masterVCF=/scratch/project_2001443/barriers_introgr_formica/vcf/filt/all_samples.DP8.hwe.AN10.noScaff00.mac2.vcf.gz
 REF=/scratch/project_2001443/reference_genome/Formica_hybrid_v1_wFhyb_Sapis.fa
+INDLIST=/scratch/project_2001443/barriers_introgr_formica/vcf/filt/all_samples.DP8.hwe.AN10.noScaff00.mac2.ind.list
 
-###UPDATE FROM HERE ON###
 # Define focal ind
-ind=$(sed -n "$SLURM_ARRAY_TASK_ID"p ind.list)
+ind=$(sed -n "$SLURM_ARRAY_TASK_ID"p $INDLIST)
+indbam=${ind%%-*} #in the bam file names, inds sequenced earlier have only numbers as ids, whereas the vcf ids (and INDLIST) contain species info for these. Match these.
 
 # 1. Extract single individual from VCF & fix floats in GQ field (or issue later when parsing with whatshap)
 bcftools view -s $ind $masterVCF -Ov | perl -npe 's/(.\/.)\:.+\:(.+\:.+\:.+\:.+\:.+\:.+\:.+)/$1\:99\:$2/' | grep -v '^Scaffold00' | bgzip > whatshap/${ind}.GQfixed.vcf.gz &&\
@@ -37,7 +38,7 @@ bcftools index -t whatshap/${ind}.GQfixed.vcf.gz
 
 # 2. Phase
 whatshap phase -o whatshap/${ind}.phased.vcf.gz --reference $REF \
-               whatshap/${ind}.GQfixed.vcf.gz $BAMDIR/${ind}_nodupl_*.bam &&\
+               whatshap/${ind}.GQfixed.vcf.gz $BAMDIR/${indbam}_nodupl_*.bam &&\
 whatshap stats whatshap/${ind}.phased.vcf.gz --tsv=whatshap/${ind}.phased.tsv
 
 ### end
