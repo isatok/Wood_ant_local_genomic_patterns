5a9cbc2e7635073d8e293bf07586ebacdafa107b
isatok <37401151+isatok@users.noreply.github.com>
2023-09-11 02:08:03 +0300

    Update 5_shapeit.sh

--- 2_Phasing/5_shapeit.sh
+++ 2_Phasing/5_shapeit.sh
@@ -3,34 +3,7 @@
 cd /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit
 for i in {01..27}; do echo "Scaffold$i"; done > scaffold.list
 
-### Run a batch job to phase the data with ShapeIt5
-
-#!/bin/bash -l
-#SBATCH -J shpt5
-#SBATCH -o /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit/logs/shpt5_Scaffold%a.out
-#SBATCH -e /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit/logs/shpt5_Scaffold%a.err
-#SBATCH --account=project_2001443
-#SBATCH -t 06:00:00
-#SBATCH -p small
-#SBATCH --array=1-27
-#SBATCH --ntasks 4
-#SBATCH --mem=8G
-
-cd /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit
-
-mychr=$(sed -n "$SLURM_ARRAY_TASK_ID"p scaffold.list)
-
-export PATH="/projappl/project_2001443/shapeit5env/bin:$PATH" 
-
-phase_common --input ../whatshap/all_samples.DP8.hwe.AN10.noScaff00.mac2.whap.vcf.gz \
-  --output "all_samples.DP8.hwe.AN10.noScaff00.mac2.whap.shapeit."$mychr".vcf.gz" \
-  --region $mychr --sequencing --thread 4 \
-  --mcmc-iterations 10b,1p,1b,1p,1b,1p,1b,1p,1b,1p,1b,1p,10m --pbwt-depth 8 --use-PS 0.0001
-
-### End of bash script
-
-
-### USE THIS, SHAPEIT4 ###
+### Run a batch job to phase the data with ShapeIt4
 
 #!/bin/bash -l
 #SBATCH -J shpt4
@@ -57,3 +30,34 @@ $SHPT4PATH/shapeit4.2 --input ../whatshap/all_samples.DP8.hwe.AN10.noScaff00.mac
 
 ### End of bash script
 5_shapeit.sh (END)
+
+
+
+------
+
+
+### In case there's a need to update to ShapeIt5, here's a script to modify further ###
+
+#!/bin/bash -l
+#SBATCH -J shpt5
+#SBATCH -o /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit/logs/shpt5_Scaffold%a.out
+#SBATCH -e /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit/logs/shpt5_Scaffold%a.err
+#SBATCH --account=project_2001443
+#SBATCH -t 06:00:00
+#SBATCH -p small
+#SBATCH --array=1-27
+#SBATCH --ntasks 4
+#SBATCH --mem=8G
+
+cd /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit
+
+mychr=$(sed -n "$SLURM_ARRAY_TASK_ID"p scaffold.list)
+
+export PATH="/projappl/project_2001443/shapeit5env/bin:$PATH" 
+
+phase_common --input ../whatshap/all_samples.DP8.hwe.AN10.noScaff00.mac2.whap.vcf.gz \
+  --output "all_samples.DP8.hwe.AN10.noScaff00.mac2.whap.shapeit."$mychr".vcf.gz" \
+  --region $mychr --sequencing --thread 4 \
+  --mcmc-iterations 10b,1p,1b,1p,1b,1p,1b,1p,1b,1p,1b,1p,10m --pbwt-depth 8 --use-PS 0.0001
+
+### End of bash script
