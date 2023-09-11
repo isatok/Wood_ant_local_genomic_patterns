d9a3f363066562e9a5ef43e373b2f25af5060336
isatok <37401151+isatok@users.noreply.github.com>
2023-09-11 22:58:57 +0300

    Update 3_Sample_table&groups_files.sh

--- 3_Prep4trees&TWISST/3_Sample_table&groups_files.sh
+++ 3_Prep4trees&TWISST/3_Sample_table&groups_files.sh
@@ -22,52 +22,22 @@ scp satokan1@puhti.csc.fi:'/scratch/project_2001443/barriers_introgr_formica/vcf
 
 #### in R - build a species-informative table from the list ####
 
-###Read in the sample list and create a data frame
-samples <- read.csv("sample.list.csv", header=F)
-samples2 <- as.data.frame(t(samples[1,]))
-
-###Create vectors of samples which belong to each species or group
-
-###Lists by species
-prat <- c("117-Fprat", "120-Fprat", "122-Fprat", "123-Fprat", "2-Fprat", "3-Fprat", "6-Fprat")
-lug <- c("1-Fprat", "108-Flug", "11-Flug", "113-Flug", "115-Flug", "124-Flug", "56-Flug", "88-Flug")
-rufa <- c("102-Frufa", "12-Frufa", "16-Frufa", "26-Frufa", "37-Frufa", "72-Frufa")
-pol <- c("CAGa_1w", "CBCH1_1w", "CBCH2_2w", "CBCH3_1w", "NAZa_1w", "VDa_1w")
-aqu <- c("10-Fpol", "103-Faqu", "104-Faqu", "106-Faqu", "107-Faqu", "109-Faqu", "114-FaquH", "13-Faqu", "17-Faqu", "22-Faqu", "24-Faqu", "25-Faqu", "27-Fpol", "30-Faqu", "31-Faqu", "32-Frufa", "38-Fpol", "42-Faqu", "44-FaquH", "47-FaquH", "48-Fpol", "50-FaquH", "51-FaquH", "58-Fpol", "62-FaquH", "66-Fpol", "67-Faqu", "70-Faqu", "76-FaquH", "77-Fpol", "83-Faqu", "9-Faqu", "91-FpolH", "93-Faqu", "99-Faqu", "CF14a_1w", "CF4b_1w", "CF8b_1w", "Pus2_1w", "CBAQ1_1w", "CBAQ2_2w", "CBAQ3_1w", "Lai_1w", #"Lai_2w", "Loa_1w")
-apr_hybr <- c("105-FaquH", "14-Fpol", "36-FpolH", "43-Fpol", "52-Fpol", "53-Fpol", "54-Frufa", "55-FaquH", "59-FpolH", "65-Frufa", "Att1_1w", "Fis2_1w", "Jar6_1w", "Lok3_1w")
-l_hybr <- c("119-Flug", "125-Flug", "126-Flug")
-
-###Lists by species and by geography
-pol_sw <- c("CAGa_1w", "CBCH1_1w", "CBCH2_2w", "CBCH3_1w", "NAZa_1w", "VDa_1w")
-aqu_swsc <- c("CBAQ1_1w", "CBAQ2_2w", "CBAQ3_1w", "Lai_1w", "Lai_2w", "Loa_1w")
-aqu_fi <- c("10-Fpol", "103-Faqu", "104-Faqu", "106-Faqu", "107-Faqu", "109-Faqu", "114-FaquH", "13-Faqu", "17-Faqu", "22-Faqu", "24-Faqu", "25-Faqu", "27-Fpol", "30-Faqu", "31-Faqu", "32-Frufa", "38-Fpol", "42-Faqu", "44-FaquH", "47-FaquH", "48-Fpol", "50-FaquH", "51-FaquH", "58-Fpol", "62-FaquH", "66-Fpol", "67-Faqu", "70-Faqu", "76-FaquH", "77-Fpol", "83-Faqu", "9-Faqu", "91-FpolH", "93-Faqu", "99-Faqu", "CF14a_1w", "CF4b_1w", "CF8b_1w", "Pus2_1w")
-prat_fi <- c("117-Fprat", "120-Fprat", "122-Fprat", "123-Fprat", "2-Fprat", "3-Fprat", "6-Fprat")
-lug_fi <- c("1-Fprat", "108-Flug", "11-Flug", "113-Flug", "115-Flug", "124-Flug", "56-Flug", "88-Flug")
-rufa_fi <- c("102-Frufa", "12-Frufa", "16-Frufa", "26-Frufa", "37-Frufa", "72-Frufa")
-apr_hybr_fi <- c("105-FaquH", "14-Fpol", "36-FpolH", "43-Fpol", "52-Fpol", "53-Fpol", "54-Frufa", "55-FaquH", "59-FpolH", "65-Frufa", "Att1_1w", "Fis2_1w", "Jar6_1w", "Lok3_1w")
-l_hybr_fi <- c("119-Flug", "125-Flug", "126-Flug")
-
-###Build the sample table
-colnames(samples2) <- c("vcfID")
-samples2$species[samples2$vcfID %in% prat] <- "pratensis"
-samples2$species[samples2$vcfID %in% lug] <- "lugubris"
-samples2$species[samples2$vcfID %in% rufa] <- "rufa"
-samples2$species[samples2$vcfID %in% pol] <- "polyctena"
-samples2$species[samples2$vcfID %in% aqu] <- "aquilonia"
-samples2$species[samples2$vcfID %in% apr_hybr] <- "aqupolrufa_hybrids"
-samples2$species[samples2$vcfID %in% l_hybr] <- "lugubris_hybrids"
-
-samples2$species_geog[samples2$vcfID %in% pol_sw] <- "polyctena_sw"
-samples2$species_geog[samples2$vcfID %in% aqu_swsc] <- "aquilonia_swsc"
-samples2$species_geog[samples2$vcfID %in% prat_fi] <- "pratensis_fi"
-samples2$species_geog[samples2$vcfID %in% lug_fi] <- "lugubris_fi"
-samples2$species_geog[samples2$vcfID %in% rufa_fi] <- "rufa_fi"
-samples2$species_geog[samples2$vcfID %in% aqu_fi] <- "aquilonia_fi"
-samples2$species_geog[samples2$vcfID %in% apr_hybr_fi] <- "aqupolrufa_hybrids_fi"
-samples2$species_geog[samples2$vcfID %in% l_hybr_fi] <- "lugubris_hybrids_fi"
-
-###Export into "sample_table.tab" to be used in making TWISST groups files
-#write.table(samples2, file="sample_table.tab", col.names=TRUE, row.names=FALSE, sep="\t", quote=FALSE)
+  library(tidyverse)
+  setwd("/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/twisst_autumn2023")
+# Read in the sample list created from the analysed vcf (Phased vcf, with F. exsecta), and create a data frame
+  samples <- read.csv("sample.list.csv", header=F, strip.white = T)
+  samples2 <- as.data.frame(t(samples[1,]))
+# Read in the sample metadata. (eg species) from the sample table used for all analyses
+  analyslist <- read.csv("/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/analysis_sample_list.txt", header=T, sep="\t")
+# Join the sample info tables and rename vcf_id
+  samples3 <- left_join(samples2, analyslist, by = c("1"="vcf_id"))
+  colnames(samples3)[1] <- "vcf_id"
+# Fill in information for exsecta
+  samples3$id[samples3$vcf_id %in% "Fexs"] <- "Fexs"
+  samples3$group[samples3$vcf_id %in% "Fexs"] <- "exsecta"
+  samples3$group_geo[samples3$vcf_id %in% "Fexs"] <- "exsecta"
+# Export into "sample_table.tab" to be used in making TWISST groups files
+  write.table(samples3, file="sample_table.tab", col.names=TRUE, row.names=FALSE, sep="\t", quote=FALSE)
 
 
 ####
