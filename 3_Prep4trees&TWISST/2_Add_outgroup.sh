a3de4cc2ad0ee4e99d2a6cf3b29e2ebd98e644f3
isatok <37401151+isatok@users.noreply.github.com>
2023-09-11 15:22:44 +0300

    Update 2_Add_outgroup.sh

--- 3_Prep4trees&TWISST/2_Add_outgroup.sh
+++ 3_Prep4trees&TWISST/2_Add_outgroup.sh
@@ -5,9 +5,9 @@
 # Create one BED file containing the positions of all phased SNPs
 # This BED file will be used as an argument to call genotypes only at these positions
 
-cd /scratch/project_2001443/vcf/phasing/shapeit
+cd /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit
 
-PHASEDVCF=/scratch/project_2001443/vcf/phasing/shapeit/all_samples.minDP8.AN10percMiss.mac2.whap.shapeit.allScafs.vcf.gz
+PHASEDVCF=/scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit/all_samples.DP8.hwe.AN10.noScaff00.mac2.whap.shapeit.allScafs.vcf.gz
 
 # Extract first 2 columns (chr - pos)
 gunzip -c $PHASEDVCF | grep -v '^#' | cut -f1,2 > phased_sites.tmp
@@ -16,7 +16,7 @@ gunzip -c $PHASEDVCF | grep -v '^#' | cut -f1,2 > phased_sites.tmp
 cut -f2 phased_sites.tmp > tmp
 paste -d '\t' phased_sites.tmp tmp > tmp2
 
-# Compute BED range (BED is 0-based)
+# Compute BED range (BED is 0-based); to insert tabs (in between ""; 2x), press [ctrl] + v + [tab]
 awk -F"  " 'BEGIN{OFS="  "} {$2=$2-1;print $0}' tmp2 > phased_sites.bed
 rm phased_sites.tmp tmp tmp2
 
@@ -26,18 +26,16 @@ rm phased_sites.tmp tmp tmp2
 ###
 
 # Instead of Freebayes, it is simpler / quicker to genotype using bcftools
-# Run on an interactive node; check that ploidy 1 flag works as expected (if a site is polymorphic, should get the major allele)
+# Run on an interactive node; (note: bcftools call --ploidy 1 retains info about both ref and alt alleles, but indicates in the GT field that it's haploid & correctly chooses the major allele)
 
-sinteractive
-cd /scratch/project_2001443/vcf/phasing/shapeit
-
+sinteractive ...
+cd /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit
 module load biokit
 
 bam=/scratch/project_2001443/Fexsecta_bam/Fexs_nodupl.bam
 ref=/scratch/project_2001443/reference_genome/Formica_hybrid_v1_wFhyb_Sapis.fa
-phbed=/scratch/project_2001443/vcf/phasing/shapeit/phased_sites.bed
-#bcftools mpileup -f $ref -R $phbed $bam | bcftools call --ploidy 1 -m -Oz -f GQ -o Fexs_nodupl_phased_sites_tmp.vcf.gz #now this may be overcommented by mistake? should indeed get a haplodised outgroup
-bcftools mpileup -f $ref -R $phbed $bam | bcftools call -m -Oz -f GQ -o Fexs_nodupl_phased_sites_tmp.vcf.gz
+phbed=/scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit/phased_sites.bed
+bcftools mpileup -f $ref -R $phbed $bam | bcftools call --ploidy 1 -m -Oz -f GQ -o Fexs_nodupl_phased_sites_tmp.vcf.gz #now this may be overcommented by mistake? should indeed get a haplodised outgroup
 
 # Edit header (instead of adding the read group, here the name of the sample will be "Fexs")
 echo "Fexs" >> out.name
@@ -45,7 +43,10 @@ bcftools reheader -s out.name Fexs_nodupl_phased_sites_tmp.vcf.gz -o Fexs_nodupl
 rm out.name Fexs_nodupl_phased_sites_tmp.vcf.gz
 
 # Combine outgroup + sample VCFs
-PHASEDVCF=/scratch/project_2001443/vcf/phasing/shapeit/all_samples.minDP8.AN10percMiss.mac2.whap.shapeit.allScafs.vcf.gz
+PHASEDVCF=/scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit/all_samples.DP8.hwe.AN10.noScaff00.mac2.whap.shapeit.allScafs.vcf.gz 
+
+bcftools index -t $PHASEDVCF 
+bcftools index -t Fexs_nodupl_phased_sites.vcf.gz
 
 bcftools merge -Ou \
   $PHASEDVCF \
