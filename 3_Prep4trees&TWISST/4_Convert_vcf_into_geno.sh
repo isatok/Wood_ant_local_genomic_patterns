bb5d0ec519c1c539b7da8291fc341dfcccfa6010
isatok <37401151+isatok@users.noreply.github.com>
2023-09-11 15:35:56 +0300

    Update 4_Convert_vcf_into_geno.sh

--- 3_Prep4trees&TWISST/4_Convert_vcf_into_geno.sh
+++ 3_Prep4trees&TWISST/4_Convert_vcf_into_geno.sh
@@ -1,22 +1,24 @@
 #parceVCF.py script:
 /scratch/project_2001443/analysis/genomics_simon/genomics_general/VCF_processing/parseVCF.py
 
+cd /scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit/
+
 #Phased vcf, with F. exsecta added in:
-FULLVCF=/scratch/project_2001443/vcf/phasing/shapeit/phased_with_outgroup_gtfix.vcf.gz
+FULLVCF=/scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit/phased_with_outgroup_gtfix.vcf.gz
 
 #Sample table (still without the outgroup, F. exsecta)
-/scratch/project_2001443/vcf/ind_lists/sample_table.tab
+sample_table.tab
 
 #Create sample table with F. exsecta
 nano fexs.tab #fexs.tab contains three columns: "Fexs  exsecta exsecta"
-cat /scratch/project_2001443/vcf/ind_lists/sample_table.tab fexs.tab > sample_table_fexs.tab
+cat sample_table.tab fexs.tab > sample_table_fexs.tab
 
 #create a ploidy table
 cut -f1 sample_table_fexs.tab | grep -v 'vcfID' | sed 's/$/ 2/' > ploidy.tab
 nano ploidy.tab #Change manually F. exsecta to "1"
-PLOIDYTAB=/scratch/project_2001443/vcf/ind_lists/ploidy.tab
+PLOIDYTAB=/scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit/ploidy.tab
 
 #make a geno file
-cd /scratch/project_2001443/vcf/geno
 PARSEPY=/scratch/project_2001443/analysis/genomics_simon/genomics_general/VCF_processing/parseVCF.py
+
 python $PARSEPY -i $FULLVCF --skipIndels --excludeDuplicates --ploidyFile $PLOIDYTAB | bgzip > phased.exsecta.geno.gz
