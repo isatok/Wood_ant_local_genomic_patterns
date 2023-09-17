454249d0a16d75b9906d34667efc6cc0b7eadf86
isatok <37401151+isatok@users.noreply.github.com>
2023-09-17 22:11:39 +0300

    Update twisst_mixedGeo_parentals_aqu_pol_rufa.sh

--- 5_TWISST/twisst_mixedGeo_parentals_aqu_pol_rufa.sh
+++ 5_TWISST/twisst_mixedGeo_parentals_aqu_pol_rufa.sh
@@ -1,35 +1,34 @@
 #!/bin/bash -l
 #SBATCH -J twisst_mixedGeo_parentals_aqu_pol_rufa_exs_og_Mi50_phyml
+#SBATCH -o /scratch/project_2001443/barriers_introgr_formica/twisst/logs/twisst_mixedGeo_parentals_aqu_pol_rufa_exs_og_Mi50_phyml.out
+#SBATCH -e /scratch/project_2001443/barriers_introgr_formica/twisst/logs/twisst_mixedGeo_parentals_aqu_pol_rufa_exs_og_Mi50_phyml.err
 #SBATCH --account=project_2001443
 #SBATCH -t 10:00:00
 #SBATCH -p small
 #SBATCH --ntasks=4
 #SBATCH --mem=8G
 
-#####CONTINUE EDITING FROM HERE FOR 2023 AUTUMN ANALYSES #####
+cd /scratch/project_2001443/barriers_introgr_formica/twisst
 
+export PATH="/projappl/project_2001443/ete3env/bin:$PATH"
 
-cd /scratch/project_2001443/analysis/twisst
-
-module load bioconda/3
-source activate ete3
-
+DATADIR=/scratch/project_2001443/barriers_introgr_formica/local_trees/phyml
+RESDIR=/scratch/project_2001443/barriers_introgr_formica/twisst/phyml
+GROUPDIR=/scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit
 
 for x in 50; do
 
 echo "Running Twisst for window size $x"
 
 python3.6 /projappl/project_2001443/twisst/twisst.py \
--t "trees/exs/phased_exs_out_max50percMi.phyml_bionj.w"$x".trees.gz" \
--w "results/parentals/exs_outgroup/parentals_fi_exs_og_max50percMi.w"$x".weights.csv.gz" \
--g pratensis_fi \
--g aquilonia_fi \
--g rufa_fi \
--g lugubris_fi \
+-t "$DATADIR/phased_exs_out_max50percMi.phyml_bionj.w"$x".trees.gz" \
+-w "$RESDIR/mixedGeo_parentals_exs_og_max50percMi.w"$x".weights.csv.gz" \
+-g aqu \
+-g rufa \
+-g pol \
 -g exsecta \
 --outgroup exsecta \
---groupsFile group_parentals_fi.tab
-
+--groupsFile $GROUPDIR/group_mixedGeo_parentals_aqu_pol_rufa.tab
 done
 
 twisst_parentals_fi_exsog_50Mi.sh (END)
