03bf4497b79e2283e0e03d7cb84cc7930cf76c35
isatok <37401151+isatok@users.noreply.github.com>
2023-11-05 19:41:36 +0200

    Update 2_pixy.sh
    Added an updated sympatric analysis

--- Pixy/2_pixy.sh
+++ Pixy/2_pixy.sh
@@ -18,6 +18,56 @@ cd /scratch/project_2001443/barriers_introgr_formica/pixy
 ### 2. Compute PIXY ------
 ###
 
+### For Finnish origin (except F. polyctena, for which it was not possible), pixy_fi.sh
+
+#!/bin/bash -l
+#SBATCH -J pixy_fi
+#SBATCH -o /scratch/project_2001443/barriers_introgr_formica/pixy/logs/pixy_fi.out
+#SBATCH -e /scratch/project_2001443/barriers_introgr_formica/pixy/logs/pixy_fi.err
+#SBATCH --account=project_2001443
+#SBATCH -t 24:00:00
+#SBATCH -p small
+#SBATCH --ntasks 8
+#SBATCH --mem=4G
+
+module load biokit
+export PATH="/projappl/project_2001443/pixyenv/bin:$PATH"
+
+cd /scratch/project_2001443/barriers_introgr_formica/pixy/
+
+pixy --stats pi dxy fst \
+--vcf /scratch/project_2001443/barriers_introgr_formica/gvfc/allsamples_filtered.vcf.gz \
+--populations groupfiles/group_parentals_FI_pixy.tab \
+--window_size 20000 \
+--output_prefix allspecies_balanced_fi \
+--n_cores 8
+
+
+# sort output - FST
+head -n1 allspecies_balanced_fi_fst.txt > tmp
+grep -v 'pop' allspecies_balanced_fi_fst.txt | sort -k3,3 -k4,4n > tmp2
+cat tmp tmp2 > allspecies_balanced_fi_fst_sort.txt
+
+# sort output - Dxy
+head -n1 allspecies_balanced_fi_dxy.txt > tmp
+grep -v 'pop' allspecies_balanced_fi_dxy.txt | sort -k3,3 -k4,4n > tmp2
+cat tmp tmp2 > allspecies_balanced_fi_dxy_sort.txt
+
+# sort output - Pi
+head -n1 allspecies_balanced_fi_pi.txt > tmp
+grep -v 'pop' allspecies_balanced_fi_pi.txt | sort -k2,2 -k3,3n > tmp2
+cat tmp tmp2 > allspecies_balanced_fi_pi_sort.txt
+
+rm tmp tmp2
+
+###This is the end
+
+#get the results for plotting
+scp satokan1@puhti.csc.fi:'/scratch/project_2001443/barriers_introgr_formica/pixy/*.txt' '/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/Pixy'
+
+
+### For mixed geographical origin, pixy_mixedgeo.sh
+
 #!/bin/bash -l
 #SBATCH -J pixy
 #SBATCH -o /scratch/project_2001443/barriers_introgr_formica/pixy/logs/pixy.out
@@ -139,7 +189,20 @@ FULLSAMPLE=/scratch/project_2001443/barriers_introgr_formica/vcf/phasing/shapeit
 INPATH=/scratch/project_2001443/barriers_introgr_formica/fst_global/groupfiles
 OUTPATH=/scratch/project_2001443/barriers_introgr_formica/pixy/groupfiles
 
-# The first try
+# The second try, with sympatric-only samples (except for F. polyctena as it's not possible)
+GROUPFILE=group_parentals_FI_pixy.tab # <--- new
+
+paste $INPATH/rufa_fi.tab <(yes "rufa" | head -n 6) > $OUTPATH/rufa_fi_6inds_pixy.tab # <--- new
+paste $INPATH/aquilonia_fi_6inds.tab <(yes "aquilonia" | head -n 6) > $OUTPATH/aquilonia_fi_6inds_pixy.tab # <--- new
+paste $INPATH/polyctena_ceu.tab <(yes "polyctena_ceu" | head -n 6) > $OUTPATH/polyctena_ceu_6inds_pixy.tab
+paste $INPATH/lugubris_fi.tab <(yes "lugubris_fi" | head -n 8) | shuf -n 6 > $OUTPATH/lugubris_fi_6inds_pixy.tab
+paste $INPATH/pratensis_fi.tab <(yes "pratensis_fi" | head -n 7) | shuf -n 6 > $OUTPATH/pratensis_fi_6inds_pixy.tab
+
+cat $OUTPATH/rufa_fi_6inds_pixy.tab $OUTPATH/aquilonia_fi_6inds_pixy.tab $OUTPATH/polyctena_ceu_6inds_pixy.tab \
+$OUTPATH/lugubris_fi_6inds_pixy.tab $OUTPATH/pratensis_fi_6inds_pixy.tab > $OUTPATH/$GROUPFILE
+
+
+# The first try (problematic - some species are sampled in different locations)
 GROUPFILE=group_parentals_all_pixy.tab
 
 paste $INPATH/rufa_all_6inds.tab <(yes "rufa" | head -n 6) > $OUTPATH/rufa_all_6inds_pixy.tab
