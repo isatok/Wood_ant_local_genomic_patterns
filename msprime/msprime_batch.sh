1a4f0b6d4530c00f1cec38c1f94034155687b028
isatok <37401151+isatok@users.noreply.github.com>
2023-11-28 10:59:23 +0200

    Create msprime_batch.sh

---
+++ msprime/msprime_batch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH -J aqu_pol
+#SBATCH -o /scratch/project_2001443/barriers_introgr_formica/msprime/logs/%a.out
+#SBATCH --account=project_2001443
+#SBATCH -t 24:00:00
+#SBATCH -p small
+#SBATCH --array=1-100
+#SBATCH --ntasks 1
+#SBATCH --mem=2G
+
+mkdir /scratch/project_2001443/barriers_introgr_formica/msprime/sim_$SLURM_ARRAY_TASK_ID
+cd  /scratch/project_2001443/barriers_introgr_formica/msprime/sim_$SLURM_ARRAY_TASK_ID
+
+python3.6 /scratch/project_2001443/barriers_introgr_formica/msprime/aqu_pol_msprime.py
