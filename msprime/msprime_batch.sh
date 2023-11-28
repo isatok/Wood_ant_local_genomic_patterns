687fd2eebf9ea9f7ad1c73d3293cbe6a74d4093f
isatok <37401151+isatok@users.noreply.github.com>
2023-11-28 11:40:26 +0200

    Update msprime_batch.sh

--- msprime/msprime_batch.sh
+++ msprime/msprime_batch.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -l
 #SBATCH -J aqu_pol
 #SBATCH -o /scratch/project_2001443/barriers_introgr_formica/msprime/logs/%a.out
+#SBATCH -e /scratch/project_2001443/barriers_introgr_formica/msprime/logs/%a.err
 #SBATCH --account=project_2001443
 #SBATCH -t 24:00:00
 #SBATCH -p small
-#SBATCH --array=1-100
+#SBATCH --array=1
 #SBATCH --ntasks 1
 #SBATCH --mem=2G
+
+#change later on --array=1-100 if the code works
 
 export PATH="/projappl/project_2001443/msprime-env/bin:$PATH"
 
 mkdir /scratch/project_2001443/barriers_introgr_formica/msprime/sim_$SLURM_ARRAY_TASK_ID
 cd  /scratch/project_2001443/barriers_introgr_formica/msprime/sim_$SLURM_ARRAY_TASK_ID
 
-python3.6 /scratch/project_2001443/barriers_introgr_formica/msprime/aqu_pol_msprime.py
+python /scratch/project_2001443/barriers_introgr_formica/msprime/aqu_pol_msprime.py
