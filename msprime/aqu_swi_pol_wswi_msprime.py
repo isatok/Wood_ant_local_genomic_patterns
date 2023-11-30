4f93c0d1556f3f3ec62da7069ef865e9afb60a67
isatok <37401151+isatok@users.noreply.github.com>
2023-11-30 09:26:18 +0200

    Update aqu_swi_pol_wswi_msprime.py

--- msprime/aqu_swi_pol_wswi_msprime.py
+++ msprime/aqu_swi_pol_wswi_msprime.py
@@ -12,7 +12,7 @@ import sys
 # An ancestral population splits into two populations ("F. aquilonia" (P1 "aqu"), and "F. polyctena" (P2 "pol")), which continue to diverge with gene flow
 
 def aqu_pol(                          ## Define a function called "aqu-pol" which has the following parameters...
-    pop_n,                 #number of analysed diploid samples per group (in this case each group is a species)  #####<-----IS THIS CORRECT?
+    pop_n,                 #number of analysed diploid samples per group (in this case each group is a species)
     pop_Ne_OG,             #outgroup Ne
     pop_Ne_Anc,            #outgroup & aqu/pol common ancestor Ne
     pop_Ne_P12_Anc,        #aqu & pol common ancestor Ne
@@ -26,7 +26,7 @@ def aqu_pol(                          ## Define a function called "aqu-pol" whic
     mig_P2P1_ancestral,    #ancestral migration rate from aqu to pol (in msprime's backwards-world the migrating lineages go from pol to aqu)
     mig_P2P1_recent,       #recent migration rate from aqu to pol (in msprime's backwards-world the migrating lineages go from pol to aqu)
     mig_P1P2_recent,       #recent migration rate from pol to aqu (in msprime's backwards-world the migrating lineages go from aqu to pol)
-    l,                     #length of genomic blocks for which window-based stats will be calculated (each block = one window) #####<-----IS THIS CORRECT?
+    l,                     #length of genomic blocks for which window-based stats will be calculated (each block = one window)
     r                      #recombination rate
 ):
     demography = msprime.Demography() ## The function executes msprime.Demography(), which is assigned to a variable "demography". 
@@ -73,22 +73,20 @@ def aqu_pol(                          ## Define a function called "aqu-pol" whic
 
 pop_Ne_Anc=500000/2            #outgroup & aqu/pol common ancestor Ne
 pop_Ne_OG=200000/2             #outgroup Ne
-pop_Ne_P12_Anc=430000/2        #aqu & pol common ancestor Ne (backw in time: before outgroup merge); 467,232 for WSwi pol, Swi aqu
+pop_Ne_P12_Anc=470000/2        #aqu & pol common ancestor Ne (backw in time: before outgroup merge)
 
-pop_Ne_resize_P1=310000/2      #aqu Ne, after resize/before clade merge (backw in time); 369,890 for WSwi pol, Swi aqu
-pop_Ne_resize_P2=210000/2      #pol Ne, after resize/before clade merge (backw in time); 215,763 for WSwi pol, Swi aqu
+pop_Ne_resize_P1=367000/2      #aqu Ne, after resize/before clade merge (backw in time)
+pop_Ne_resize_P2=216000/2      #pol Ne, after resize/before clade merge (backw in time)
 
-pop_Ne_P1=52000/2              #the initial aqu Ne; 23,245 for WSwi pol, Swi aqu
-pop_Ne_P2=280000/2             #the initial pol Ne; 32,522 for WSwi pol, Swi aqu
-
-t_parents=225000               #timing of aqu & pol split (i.e. merge backw in time); 212,802 for WSwi pol, Swi aqu
+pop_Ne_P1=23000/2              #the initial aqu Ne
+pop_Ne_P2=33000/2              #the initial pol Ne
+t_parents=213000               #timing of aqu & pol split (i.e. merge backw in time)
 t_outgroup=2000000             #timing of outgroup & aqu/pol ancestor split (i.e. merge backw in time). In theory, around 2e6 gens: 5Mya (Goropash. 2012) / 2.5 years per generation
 
-t_resize=7500                  #timing of aqu & pol resize; 6,540 for WSwi pol, Swi aqu
+t_resize=6500                  #timing of aqu & pol resize
 
-mig_P2P1_ancestral=5.99e-6     #ancestral migration rate from aqu to pol (in msprime's backwards-world the migrating lineages go from pol to aqu)
-mig_P2P1_recent=1.14e-5        #recent migration rate from aqu to pol (in msprime's backwards-world the migrating lineages go from pol to aqu)
-mig_P1P2_recent=4.02e-6        #recent migration rate from pol to aqu (in msprime's backwards-world the migrating lineages go from aqu to pol)
+mig_P2P1_ancestral=2.36e-6     #ancestral migration rate from aqu to pol (in msprime's backwards-world the migrating lineages go from pol to aqu). Ne(pol)/m = 0.51/215,763=0.0000023637 
+mig_P2P1_recent=3.69e-6        #recent migration rate from aqu to pol (in msprime's backwards-world the migrating lineages go from pol to aqu). Ne(pol)/m = 0.12/32,522=0.0000036898
 
 pop_n=10                       #number of analysed diploid samples per group (in this case each group is a species)
 r=1e-6                         #recombination rate in centimorgans per basepair (equals to 1cM/Mb)
