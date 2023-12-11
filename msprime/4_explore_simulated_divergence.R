b3a156836db000ae4e80a1524fc5979a33e684ca
isatok <37401151+isatok@users.noreply.github.com>
2023-12-12 01:13:40 +0200

    Create 4_explore_simulated_divergence.R

---
+++ msprime/4_explore_simulated_divergence.R
@@ -0,0 +1,7 @@
+### This script is for exploring the simulated divergence computed by PIXY. 
+#The input files are from /scratch/project_2001443/barriers_introgr_formica/msprime/pixy/*txt (fst, dxy, pi)
+
+scp satokan1@puhti.csc.fi:'/scratch/project_2001443/barriers_introgr_formica/msprime/pixy/*txt' \
+'/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/Pixy/simulated_divergence'
+
+UPDATE THIS WHEN R-script is ready
