2acf11b636237c58f770ce2e60124ed8c75b096c
isatok <37401151+isatok@users.noreply.github.com>
2024-01-24 13:11:59 +0200

    Update allsites_depth.sh

--- Pixy/allsites_depth.sh
+++ Pixy/allsites_depth.sh
@@ -1,3 +1,122 @@
+### Compute bam coverage for the highcov individuals used in Jan 2024 ###
+
+cd /scratch/project_2001443/bam/cov_highcovInds_5perSp
+
+#make a list with filenames and bampaths
+
+file=/scratch/project_2001443/bam/cov_highcovInds_5perSp/bam.highcovinds.list 
+
+#This is the order of the individuals as how they are in the cleaned coverage file (according to the order in ind_cov.list created later)
+#ind_cov_species.list:
+chr chr
+start start
+end end
+102-Frufa rufa
+104-Faqu aqu
+106-Faqu aqu
+107-Faqu aqu
+108-Flug lug
+109-Faqu aqu
+113-Flug lug
+115-Flug lug
+117-Fprat prat
+11-Flug lug
+120-Fprat prat
+122-Fprat prat
+123-Fprat prat
+124-Flug lug
+16-Frufa rufa
+26-Frufa rufa
+37-Frufa rufa
+44-FaquH aqu
+6-Fprat prat
+72-Frufa rufa
+CAGa_1w pol
+CBCH1_1w pol
+CBCH3_1w pol
+NAZa_1w pol
+VDa_1w pol
+
+
+
+###
+### Compute sequencing depth per sample (after duplicate removal) ------------------------------------------------------------------------
+###
+
+
+
+#!/bin/bash -l
+#SBATCH -J mosdepth_highcovinds
+#SBATCH -o /scratch/project_2001443/bam/cov_highcovInds_5perSp/logs/mosdepth_highcovinds%j.out
+#SBATCH -e /scratch/project_2001443/bam/cov_highcovInds_5perSp/logs/mosdepth_highcovinds%j.err
+#SBATCH --account=project_2001443
+#SBATCH -t 04:00:00
+#SBATCH -p small
+#SBATCH --array=1-25
+#SBATCH --ntasks 1
+#SBATCH --mem-per-cpu=4GB
+
+export PATH="/projappl/project_2001443/bioinfo_1222_env/bin:$PATH"
+
+BAMDIR=/scratch/project_2001443/barriers_introgr_formica/bam/bam_all
+FINALDIR=/scratch/project_2001443/bam/cov_highcovInds_5perSp
+FILELIST=/scratch/project_2001443/bam/cov_highcovInds_5perSp/bam.highcovinds.list
+NAMELIST=/scratch/project_2001443/bam/cov_highcovInds_5perSp/nodupl_highcovinds_numonly.list
+
+cd /scratch/project_2001443/bam/cov_highcovInds_5perSp
+
+# Get file
+file=$(sed -n "$SLURM_ARRAY_TASK_ID"p $FILELIST)
+
+# Get sample ID
+sample=$(sed -n "$SLURM_ARRAY_TASK_ID"p $NAMELIST)
+
+mosdepth -t 1 -b 100000 -n $FINALDIR/${sample}_overlap_correction $BAMDIR/$sample"_nodupl_wRG_clip.bam"
+mosdepth -t 1 -b 100000 -n $FINALDIR/${sample}_overlap_correction $BAMDIR/$sample"_nodupl_wRG.bam"
+
+### END
+
+### ------------------------------------------------------------------------
+
+
+### Get average coverage per each window ----------------
+
+cd /scratch/project_2001443/bam/cov_highcovInds_5perSp
+
+#paste per region coverage data for all pixy samples
+touch output.txt
+touch ind_cov.list
+for i in *regions.bed.gz; do
+    zcat "$i" | awk -v OFS='\t' '{print $4}' | paste - output.txt > temp_output.txt
+    mv temp_output.txt output.txt
+    echo "Processed $i"
+    echo $i >> ind_cov.list
+done
+
+#paste region info as the first column
+zcat 108_overlap_correction.regions.bed.gz | awk -v OFS='\t' '{print $1, $2, $3}' > 1stcol.txt #extract region info
+paste 1stcol.txt output.txt > mosdepth_highcovinds_100kb_regions_depth.txt
+rm 1stcol.txt output.txt
+
+#make the ind_cov.list to have species vcf_id as 1st col and species as 2nd col
+nano ind_cov.list
+#modify it; resulting in ind_cov_species.list that has 1st row: chr, 2nd row: start, 3rd row: end, followed by ind/species info
+
+#remove regions that are not used: mitchondria (mtDNA), Wolbachia (wFhyb*), and Spiroplasma (Spiroplasma*), as well as Scaffold00 and Scaffold03
+grep -v -e mtDNA -e wFhyb -e Spiroplasma -e Scaffold00 -e Scaffold03 mosdepth_highcovinds_100kb_regions_depth.txt > mosdepth_highcovinds_100kb_regions_depth_coreregions.txt ; rm mosdepth_highcovinds_100kb_regions_depth.txt
+
+#get the files  mosdepth_highcovinds_100kb_regions_depth_coreregions.txt, ind_cov_species.list
+
+scp satokan1@puhti.csc.fi:'/scratch/project_2001443/bam/cov_highcovInds_5perSp/mosdepth_highcovinds_100kb_regions_depth_coreregions.txt' \
+'/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/Correlations_whole_genome/depth'
+scp satokan1@puhti.csc.fi:'/scratch/project_2001443/bam/cov_highcovInds_5perSp/ind_cov_species.list' \
+'/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/Correlations_whole_genome/depth'
+
+
+
+
+
+
 
 # 4b_bam_coverage_forPIXY.sh
 
