bfb08b6f0b9e3bfd8a1ae2e0e15deb8359b4da06
isatok <37401151+isatok@users.noreply.github.com>
2024-12-19 14:20:25 +0200

    Update 2_pixy.sh

--- Pixy/2_pixy.sh
+++ Pixy/2_pixy.sh
@@ -18,13 +18,45 @@ cd /scratch/project_2001443/barriers_introgr_formica/pixy
 ### 2. Compute PIXY ------
 ###
 
-###Additional run on 18.12.2024: use the same 250SNP windows as for fd (60% overlap), to have comparable estimates for introgression and PIXY
+###Additional runs on 19.12.2024: 
+### A) use the same 250SNP windows as for fd (60% overlap), to have comparable estimates for introgression and PIXY
 ###values across the genome.
+### B) use smaller fixed window sizes, 50 kb, to demonstrate how the whole-genome correlations react to this.
+
 
 #Create a bed-file with fd windows
 #...
 
+### B)
+cd /scratch/project_2001443/barriers_introgr_formica_112024/pixy
 
+#!/bin/bash -l
+#SBATCH -J pixy_50kb_highcov_5inds
+#SBATCH -o /scratch/project_2001443/barriers_introgr_formica_112024/pixy/logs/pixy_50kb_highcov_5inds.out
+#SBATCH -e /scratch/project_2001443/barriers_introgr_formica_112024/pixy/logs/pixy_50kb_highcov_5inds.err
+#SBATCH --account=project_2001443
+#SBATCH -t 24:00:00
+#SBATCH -p small
+#SBATCH --ntasks 8
+#SBATCH --mem=4G
+
+module load biokit
+export PATH="/projappl/project_2001443/pixyenv/bin:$PATH"
+
+cd /scratch/project_2001443/barriers_introgr_formica_112024/pixy/
+
+pixy --stats pi dxy fst \
+--vcf /scratch/project_2001443/barriers_introgr_formica_112024/gvcf/highcovsamples_filtered_noindels_rmdup.vcf.gz \
+--populations groupfiles/group_pixy_100kb_5inds_highcovsamples.tab \
+--window_size 50000 \
+--output_prefix pixy_50kb_highcov_5inds \
+--n_cores 8
+
+#END.
+
+
+#get the results for plotting
+scp satokan1@puhti.csc.fi:'/scratch/project_2001443/barriers_introgr_formica_112024/pixy/pixy_50kb_highcov_5inds*.txt' '/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/Pixy'
 
 
 ###ONCE MORE 21.1.2024, with updated groupsfile to be similar w "simple" Fbranch & updated highcov vcf file###
@@ -59,5 +91,5 @@ pixy --stats pi dxy fst \
 
 
 #get the results for plotting
-scp satokan1@puhti.csc.fi:'/scratch/project_2001443/barriers_introgr_formica/pixy/pixy_100kb_5inds*.txt' '/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/Pixy'
+scp satokan1@puhti.csc.fi:'/scratch/project_2001443/barriers_introgr_formica/pixy/pixy_100kb_highcov_5inds*.txt' '/Users/inaukkar/Library/CloudStorage/OneDrive-UniversityofHelsinki/PhD/4_formica_local_genomics/Pixy'
 
